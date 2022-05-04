298e977f37d6c4ea20355104127101559dcf1042
Bhartiverma1989 <104622073+Bhartiverma1989@users.noreply.github.com>
2022-05-04 14:19:41 +0530

    this is my commit
    completed task
    please review

---
+++ JanBatchVersion1/src/main/java/myPackage/BasicClass.java
@@ -0,0 +1,14 @@
+package myPackage;
+
+public class BasicClass {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("this is addition of two integer "+c);
+
+		
+	}
+
+}
